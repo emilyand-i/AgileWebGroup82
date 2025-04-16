0c26d328ed41ccaf87f31436f8e23906d17f79ca
emilyand-i <emzymwillis@gmail.com>
2025-04-16 16:27:32 +0800

    signup box/ popup

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+function showLogin() {
+  document.getElementById("auth-form").style.display = 'table-cell';
+}
+
+function exitLogin() {
+  document.getElementById("auth-form").style.display = 'none';
+}
+
+
