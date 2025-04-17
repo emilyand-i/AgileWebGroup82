8eb2786a644c7edaad00fd258364c0430e5d83c0
emilyand-i <emzymwillis@gmail.com>
2025-04-17 15:49:31 +0800

    x-scroll info boxes added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 
 
 function showLogin() {
-  document.getElementById("auth-form").style.display = 'table-cell';
+  document.getElementById("auth-form").style.display = 'flex';
+  document.getElementById("main").style.display = 'none';
 }
 
 function exitLogin() {
   document.getElementById("auth-form").style.display = 'none';
+  document.getElementById("main").style.display = 'initial';
 }
 
 
