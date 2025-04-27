5611c23bdd4eecefc454dc7fb43a7f31992a127f
emilyand-i <emzymwillis@gmail.com>
2025-04-27 11:41:37 +0800

    backend structure implemented

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+function flipForm() {
+  document.getElementById("form-wrapper").classList.toggle("flip");
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.signin-btn').addEventListener('click', flipForm);
+});
+
+
