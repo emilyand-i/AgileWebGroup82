6542d3c4dd987188771f1fd94e38bcf449999f8b
emilyand-i <emzymwillis@gmail.com>
2025-04-27 12:18:36 +0800

    deleted unwanted files

--- js/script.js
+++
@@ -1,9 +0,0 @@
-function flipForm() {
-  document.getElementById("form-wrapper").classList.toggle("flip");
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('.signin-btn').addEventListener('click', flipForm);
-});
-
-
