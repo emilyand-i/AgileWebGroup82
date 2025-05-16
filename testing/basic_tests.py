3a3d929e325dd1a4f41550ad1e32f51230f150b9
Luke <23814635@student.uwa.edu.au>
2025-05-16 16:16:09 +0800

    New Tests Created focues on Intro View of the app
    SetUp, TearDown and homePageLoad test all cleaned up to better suit our app's
    first page.

--- testing/basic_tests.py
+++ testing/basic_tests.py
@@ -1,16 +1,21 @@
 import unittest
 from app import create_app, user_db
+from app.models import User
 
 
-#examples of tests to be run on our app
+#Basic Checks intended for the introductary view (index.html)
 class BasicTestCase(unittest.TestCase):
+    # Create a test version of your app with the test configuration
     def setUp(self):
-        self.app = create_app('test')  # Pass a config name if you have factory
+        self.app = create_app('test')  # REPLACE WITH  config name if we use have factory done the road
         self.client = self.app.test_client()
 
+        #set the context of the app
         with self.app.app_context():
             user_db.create_all()
 
+
+    # Function to clean up after each test (keep the tests isolated)
     def tearDown(self):
         with self.app.app_context():
             user_db.session.remove()
@@ -19,8 +24,11 @@ class BasicTestCase(unittest.TestCase):
     def test_homepage_loads(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+        #Checks that the phrase “Welcome to Plantly!” appears somewhere in the page’s HTML
+
 
     #an example more related to our app currently
+    #THIS IS RELATED TO SHAREBOARD AND SHOULD BE MOVED TO A DIFFERENT TEST FILE
     def test_update_social(self):
         response = self.client.get('/api/update-social')
         self.assertEqual(response.status_code, 200)
@@ -28,3 +36,9 @@ class BasicTestCase(unittest.TestCase):
         self.assertIn('public_posts', data)
         self.assertIsInstance(data['public_posts'], list)
 
+
+
+
+
+
+    
--- testing/config.py
+++ testing/config.py
@@ -1,12 +1,15 @@
 #very basic setup for testing
 # NOT TO BE USED IN FINAL OR SUBMITTED LIKE THIS!!!!!!!!!!!!
+
+
 class TestConfig:
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # In-memory DB
-    WTF_CSRF_ENABLED = False
+    WTF_CSRF_ENABLED = False #should also have tests where this is true
     SECRET_KEY = 'test_secret'
 
 
 #How to get started: 
+#may need to install FACTORY - as seen in the lectures
 # pip install pytest flask-testing
 # pytest tests/ (to run all current tests - Note they probably won't work at this stage in time)
