f139710edf74b49b506c2e55173e9e91934e908d
emilyand-i <emzymwillis@gmail.com>
2025-05-17 20:58:32 +0800

    wip

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ csrf = CSRFProtect()
 def create_app():
   app = Flask(__name__, static_folder = 'static') # app is our web server now
   app.config['SECRET_KEY'] = 'AgileWeb_group82' # secret key for encryption and security
-  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db' # create database called 'users.db' in this folder to store user accounts
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db' # create database called 'users.db' in this folder to store user accounts
   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
   
   user_db.init_app(app) # Connect database object to flask app
